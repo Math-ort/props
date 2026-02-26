c468a4cc35dc8bf77b8b9de4ce7a7f8d8be321c4
mathias ortiz <math@mathiass-MacBook-Air.local>
2026-02-26 09:20:25 +0100

    ejercicio terminado

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 import './App.css'
+import { useState } from "react";
+import Task from "./components/Task";
+import AddTaskForm from "./components/AddTaskForm";
 
 const App = () => {
   const [tasks, setTasks] = useState([
@@ -7,10 +10,41 @@ const App = () => {
     { id: 3, text: 'Hacer ejercicio', completed: false }
   ]);
 
-  
+  //para añadir una tarea:
+  const addTask = (text) => {
+    const newTask = {
+      id: tasks.length +1,
+      text,
+      completed: false
+    };
+    setTasks([...tasks, newTask]); 
+  };
+
+  // para eliminar una tarea:
+  const deleteTask = (id) => {
+    setTasks(tasks.filter(task => task.id !== id));
+  };
+
+  // para marcar una tarea como completada:
+const toggleTask = (id) => {
+  setTasks(tasks.map(task => task.id === taskid ? { ...task, completed: !task.completed } : task
+    )
+  );   
+  }; 
 
   return (
-    <></>
+    <div>
+      <h1>Lista de Tareas</h1>
+      <AddTaskForm addTask={addTask} />
+      {tasks.map(task => (
+        <Task 
+          key={task.id} 
+          task={task} 
+          deleteTask={deleteTask} 
+          toggleTask={toggleTask} 
+        />
+      ))}
+    </div>
   );
 };
 
